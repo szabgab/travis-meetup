57f7bf59c1c1865c2d236128ee8a39934b49e4e4
Gabor Szabo <gabor@szabgab.com>
2020-08-16 10:27:49 +0300

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -1,2 +1,14 @@
-def test_anything():
+import os
+import pytest
+
+print(f"DATABASE = {os.environ['DATABASE']}")
+
+@pytest.mark.postgresql
+def test_postgresql():
+    print("Testing postgresql")
+    pass
+
+@pytest.mark.mysql
+def test_mysql():
+    print("Testing mysql")
     pass
